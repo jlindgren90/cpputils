6cab583115343d756792e6b4a03784a725a9c5cd
John Lindgren <john@jlindgren.net>
2025-07-24 03:15:32 -0400

    Add util::next_after/next_after_if

--- util.h
+++ util.h
@@ -26,16 +26,16 @@ namespace util
 
 /* Shorthand for std::find */
 template<typename L, typename V>
-auto find(L &list, V &&val)
+auto find(L &list, const V &val)
 {
-    return std::find(list.begin(), list.end(), std::forward<V>(val));
+    return std::find(list.begin(), list.end(), val);
 }
 
 /* Shorthand for std::find_if */
 template<typename L, typename F>
-auto find_if(L &list, F &&func)
+auto find_if(L &list, const F &func)
 {
-    return std::find_if(list.begin(), list.end(), std::forward<F>(func));
+    return std::find_if(list.begin(), list.end(), func);
 }
 
 /* Shorthand for std::remove + std::erase */
@@ -52,6 +52,35 @@ void remove_if(L &list, F &&func)
 {
     list.erase(std::remove_if(list.begin(), list.end(), std::forward<F>(func)),
                list.end());
+}
+
+template<typename It, typename V>
+auto next_after(It start, It stop, const V &val, bool wrap)
+{
+    auto cur = std::find(start, stop, val);
+    if (cur == stop) {
+        return start;
+    }
+    auto next = cur;
+    if (++next == stop && wrap && cur != start) {
+        next = start;
+    }
+    return next;
+}
+
+template<typename It, typename V, typename F>
+auto next_after_if(It start, It stop, const V &val, bool wrap, const F &func)
+{
+    auto cur = std::find(start, stop, val);
+    if (cur == stop) {
+        return std::find_if(start, stop, func);
+    }
+    auto next = cur;
+    next = std::find_if(++next, stop, func);
+    if (next == stop && wrap) {
+        next = std::find_if(start, cur, func);
+    }
+    return next;
 }
 
 /* Generic implementation of operator=() using constructor */
